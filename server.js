291b6f337de195db6304bf183f77c344cab10ed9
Jason Butterfield <jasonbu@gmail.com>
2021-02-19 16:08:45 -0500

    Update Parcel-Bundle

--- server.js
+++ server.js
@@ -24,18 +24,18 @@ mongoose
     useUnifiedTopology: true,
   })
   .then((con) => {
-    // console.log('DB Connection made');
+    console.log('DB Connection made');
   });
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  // console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}...`);
 });
 
 // UNHANDLED REJECTION
 process.on('unhandledRejection', (err) => {
-  // console.log(err.name, err.message);
-  // console.log('UNHANDLED REJECTION - Shutting Down App');
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION - Shutting Down App');
   server.close(() => {
     process.exit(1);
   });
